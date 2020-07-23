ae60817e2af3243e1831a7f1fa8d8d020e0f8481
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-23 10:33:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    num = 0
+    count_string = self.split(/[.+!+?+]/)
+    count_string.length
+    binding.pry
   end
 end
+
--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -35,7 +35,9 @@ describe String do
   describe "#count_sentences" do
 
     it  "returns the number of sentences in a string" do
-      expect("one. two. three?".count_sentences).to eq(3)
+       complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+      expect(complex_string.count_sentences).to eq(4)
+      #expect("one. two. three?".count_sentences).to eq(3)
     end
 
     it "returns zero if there are no sentences in a string" do
